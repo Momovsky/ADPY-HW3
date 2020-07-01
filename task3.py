9acf39fb6d17543ff42f98689d7e3b3f9aec5785
Igor <63199449+Momovsky@users.noreply.github.com>
2020-07-01 13:41:28 +0300

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,18 +1,18 @@
 def adv_print(*args, **kwargs):
-  text = list(*args)
-  for key, value in kwargs.items():
-    if key == 'start':
-      start_offset = 0
-      for letter in value:
-        text.insert(start_offset, letter)
-        start_offset += 1
-    if key == 'max_line':
-      max_line_offset = value
-      while len(text)>value:
-        text.insert(value, '\n')
-        value+=max_line_offset
-    if key == 'in_file':
-      if value == True:
-        with open('test.txt', 'w', encoding='utf-8') as file:
-          file.write(''.join(text))
+    text = list(*args)
+    for key, value in kwargs.items():
+        if key == 'start':
+            start_offset = 0
+            for letter in value:
+                text.insert(start_offset, letter)
+                start_offset += 1
+        if key == 'max_line':
+            max_line_offset = value
+            while len(text)>value:
+                text.insert(value, '\n')
+                value+=max_line_offset
+        if key == 'in_file':
+            if value == True:
+                with open('test.txt', 'w', encoding='utf-8') as file:
+                    file.write(''.join(text))
   print(''.join(text))
