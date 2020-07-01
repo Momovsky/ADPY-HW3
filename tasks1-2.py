65dc9adde991efa192fc928bd89472098cf3df61
Igor <63199449+Momovsky@users.noreply.github.com>
2020-07-01 13:38:46 +0300

    Update tasks1-2.py

--- tasks1-2.py
+++ tasks1-2.py
@@ -1,39 +1,39 @@
 class Contact:
-  def __init__(self, name, surname, telephone, fav_cont=False, *args, **kwargs):
-    self.name = name
-    self.surname = surname
-    self.telephone = telephone
-    self.fav_cont = fav_cont
-    self.args = args
-    self.kwargs = [(f'{key}: {item}') for key, item in kwargs.items()]
+    def __init__(self, name, surname, telephone, fav_cont=False, *args, **kwargs):
+        self.name = name
+        self.surname = surname
+        self.telephone = telephone
+        self.fav_cont = fav_cont
+        self.args = args
+        self.kwargs = [(f'{key}: {item}') for key, item in kwargs.items()]
 
   def __str__(self):
-    my_n = '\n\t'
-    return(f'Имя: {self.name}\nФамилия: {self.surname}\nТелефон: {self.telephone}\nВ избранных: {"да" if self.fav_cont else "нет"}\nДополнительная информация:{my_n + ", ".join(self.args) if self.args else ""}{my_n + my_n.join(item for item in self.kwargs) if self.kwargs else ""}{my_n + "Нет" if not self.args and not self.kwargs else ""}')
+      my_n = '\n\t'
+      return(f'Имя: {self.name}\nФамилия: {self.surname}\nТелефон: {self.telephone}\nВ избранных: {"да" if self.fav_cont else "нет"}\nДополнительная информация:{my_n + ", ".join(self.args) if self.args else ""}{my_n + my_n.join(item for item in self.kwargs) if self.kwargs else ""}{my_n + "Нет" if not self.args and not self.kwargs else ""}')
 
 class PhoneBook:
-  def __init__(self, name):
-    self.name = name
-    self.contact_list = []
+    def __init__(self, name):
+        self.name = name
+        self.contact_list = []
 
   def contact_output(self):
-    for Contact in self.contact_list:
-      print(Contact)
+      for Contact in self.contact_list:
+          print(Contact)
   
   def add_contact(self, Contact):
-    self.contact_list.append(Contact)
+      self.contact_list.append(Contact)
   
   def del_by_number(self, number):
-    for Contact in self.contact_list:
-      if Contact.telephone == number:
-        self.contact_list.remove(Contact)
+      for Contact in self.contact_list:
+          if Contact.telephone == number:
+              self.contact_list.remove(Contact)
 
   def find_favs(self):
-    for Contact in self.contact_list:
-      if Contact.fav_cont == True:
-        print(Contact.telephone)
+      for Contact in self.contact_list:
+          if Contact.fav_cont == True:
+              print(Contact.telephone)
 
   def find_contact(self, name, surname):
-    for Contact in self.contact_list:
-      if Contact.name == name and Contact.surname == surname:
-        print(Contact.telephone)
+      for Contact in self.contact_list:
+          if Contact.name == name and Contact.surname == surname:
+              print(Contact.telephone)
